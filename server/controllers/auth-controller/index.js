2a5aa4bc9e155a8a1eb8d9d1ce9a18e80ba36777
Kushal1715 <kushalkhadka77@gmail.com>
2025-02-11 20:53:58 +0545

    making user model and regiter user controller

---
+++ server/controllers/auth-controller/index.js
@@ -0,0 +1,39 @@
+const User = require("../../models/User");
+const bcrypt = require("bcryptjs");
+
+const registerUser = async (req, res) => {
+  try {
+    const { userName, email, password, role } = req.body;
+
+    const existingUser = await User.findOne({ $or: [{ userName }, { email }] });
+
+    if (existingUser) {
+      return res.status(400).json({
+        success: false,
+        message: "username or email already exists",
+      });
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const newUser = new User({
+      userName,
+      email,
+      password: hashedPassword,
+      role,
+    });
+
+    await newUser.save();
+
+    res.status(201).json({
+      success: true,
+      message: "user registered successfully",
+    });
+  } catch (e) {
+    console.log(e);
+    return res.status(500).json({
+      success: false,
+      message: "something went wrong",
+    });
+  }
+};
