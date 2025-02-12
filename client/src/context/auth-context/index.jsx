125ce3649fdad142e4502f3a7d0b74b30dfb2392
Kushal1715 <kushalkhadka77@gmail.com>
2025-02-12 22:21:19 +0545

    handling loading state

--- client/src/context/auth-context/index.jsx
+++ client/src/context/auth-context/index.jsx
@@ -1,3 +1,4 @@
+import { Skeleton } from "@/components/ui/skeleton";
 import { signInInitialValue, signUpInitialValue } from "@/config";
 import { checkAuthService, loginService, registerService } from "@/services";
 import { isAxiosError } from "axios";
@@ -8,6 +9,7 @@ export const AuthContext = createContext(null);
 const AuthProvider = ({ children }) => {
   const [signInFormData, setSignInFormData] = useState(signInInitialValue);
   const [signUpFormData, setSignUpFormData] = useState(signUpInitialValue);
+  const [loading, setLoading] = useState(true);
   const [auth, setAuth] = useState({
     isAuthenticated: false,
     user: null,
@@ -39,18 +41,31 @@ const AuthProvider = ({ children }) => {
   };
 
   const checkAuth = async () => {
-    const data = await checkAuthService();
-    console.log(data);
-    if (data?.success) {
-      setAuth({
-        isAuthenticated: true,
-        user: data?.data?.user,
-      });
-    } else {
-      setAuth({
-        isAuthenticated: false,
-        user: null,
-      });
+    try {
+      const data = await checkAuthService();
+      console.log(data);
+      if (data?.success) {
+        setAuth({
+          isAuthenticated: true,
+          user: data?.data?.user,
+        });
+        setLoading(false);
+      } else {
+        setAuth({
+          isAuthenticated: false,
+          user: null,
+        });
+        setLoading(false);
+      }
+    } catch (e) {
+      console.log(e);
+      if (!e?.response?.data?.success) {
+        setAuth({
+          isAuthenticated: false,
+          user: null,
+        });
+        setLoading(false);
+      }
     }
   };
 
@@ -72,7 +87,8 @@ const AuthProvider = ({ children }) => {
         auth,
       }}
     >
-      {children}
+      {loading ? <Skeleton /> : children}
+      {/* {children} */}
     </AuthContext.Provider>
   );
 };
