d9b8373750e8c207982890833d98dd1d4bafc9ed
Kushal1715 <kushalkhadka77@gmail.com>
2025-02-12 18:30:00 +0545

    making route guard

---
+++ client/src/components/route-guard/index.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import { Navigate, useLocation } from "react-router-dom";
+
+const RouteGuard = ({ isAuthenticated, user, element }) => {
+  const location = useLocation();
+
+  if (!isAuthenticated && !location.pathname.includes("/auth")) {
+    return <Navigate to="/auth" />;
+  }
+
+  if (
+    isAuthenticated &&
+    user.role === "instructor" &&
+    !location.pathname.includes("/instructor")
+  ) {
+    return <Navigate to="/instructor" />;
+  }
+
+  if (
+    isAuthenticated &&
+    !user.role === "instructor" &&
+    (location.pathname.includes("/instructor") ||
+      location.pathname.includes("/auth"))
+  ) {
+    return <Navigate to="/home" />;
+  }
+
+  return <>{element}</>;
+};
+
+export default RouteGuard;
