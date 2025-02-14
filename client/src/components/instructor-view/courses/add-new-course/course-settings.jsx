04b889d22cfb734c9e55577ef73f88a1c7601145
Kushal1715 <kushalkhadka77@gmail.com>
2025-02-14 12:14:57 +0545

    making ui for course-settings

--- client/src/components/instructor-view/courses/add-new-course/course-settings.jsx
+++ client/src/components/instructor-view/courses/add-new-course/course-settings.jsx
@@ -1,7 +1,22 @@
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
 import React from "react";
 
 const CourseSettings = () => {
-  return <div>CourseSettings</div>;
+  return (
+    <Card>
+      <CardHeader>
+        <CardTitle>Course Settings</CardTitle>
+      </CardHeader>
+      <CardContent>
+        <div>
+          <Label>Upload Course Image</Label>
+          <Input type="file" accept="image/*" className="mt-4 cursor-pointer" />
+        </div>
+      </CardContent>
+    </Card>
+  );
 };
 
 export default CourseSettings;
