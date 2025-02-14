8ecf4f7891bc74027667e5ba58736161e31386a8
Kushal1715 <kushalkhadka77@gmail.com>
2025-02-14 15:56:03 +0545

    making ui of course curriculum

--- client/src/components/instructor-view/courses/add-new-course/course-curriculum.jsx
+++ client/src/components/instructor-view/courses/add-new-course/course-curriculum.jsx
@@ -1,7 +1,64 @@
-import React from "react";
+import { Button } from "@/components/ui/button";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { Switch } from "@/components/ui/switch";
+import { courseCurriculumInitialFormData } from "@/config";
+import { InstructorContext } from "@/context/instructor-context";
+import React, { useContext } from "react";
 
 const CourseCurriculum = () => {
-  return <div>CourseCurriculum</div>;
+  const { courseCurriculumFormData, setCourseCurriculumFormData } =
+    useContext(InstructorContext);
+
+  const handleAddLecture = () => {
+    setCourseCurriculumFormData([
+      ...courseCurriculumFormData,
+      { ...courseCurriculumInitialFormData[0] },
+    ]);
+  };
+  return (
+    <Card>
+      <CardHeader>
+        <CardTitle>Create Course Curriculum</CardTitle>
+      </CardHeader>
+      <CardContent>
+        <Button onClick={handleAddLecture}>Add Lecture</Button>
+        <div className="mt-4 space-y-4">
+          {courseCurriculumFormData.map((formData, index) => (
+            <Card className="p-6" key={index}>
+              <CardContent>
+                <div className="flex justify-between items-center w-full">
+                  <div className="">
+                    <Label className="text-lg font-bold">
+                      Lecture {index + 1}
+                    </Label>
+                    <Input
+                      type="text"
+                      placeholder={`Enter lecture ${index + 1} title`}
+                      name={`lecture ${index + 1}`}
+                      className="max-w-80 lg:w-80"
+                    />
+                  </div>
+                  <div className="flex items-center space-x-2">
+                    <Switch id={`lecture ${index + 1}`} />
+                    <Label htmlFor={`lecture ${index + 1}`}>Free Preview</Label>
+                  </div>
+                </div>
+                <div className="mt-6">
+                  <Input
+                    type="file"
+                    accept="/video*"
+                    className="cursor-pointer"
+                  />
+                </div>
+              </CardContent>
+            </Card>
+          ))}
+        </div>
+      </CardContent>
+    </Card>
+  );
 };
 
 export default CourseCurriculum;
