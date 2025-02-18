88b5193674012e7acbb8848b87c591f35203f58b
Kushal1715 <kushalkhadka77@gmail.com>
2025-02-18 13:13:25 +0545

    making video player

--- client/src/components/video-player/index.jsx
+++ client/src/components/video-player/index.jsx
@@ -1,7 +1,12 @@
 import React from "react";
+import ReactPlayer from "react-player";
 
 const VideoPlayer = ({ url }) => {
-  return <div>VideoPlayer</div>;
+  return (
+    <div>
+      <ReactPlayer height={"100%"} width={"100%"} url={url} />
+    </div>
+  );
 };
 
 export default VideoPlayer;
