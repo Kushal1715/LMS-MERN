f7559e011a93444fdfa6b369814496064e9edc65
Kushal1715 <kushalkhadka77@gmail.com>
2025-02-18 15:29:19 +0545

    making video-player component

--- client/src/components/video-player/index.jsx
+++ client/src/components/video-player/index.jsx
@@ -1,10 +1,16 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 import ReactPlayer from "react-player";
 
 const VideoPlayer = ({ url }) => {
   return (
-    <div>
-      <ReactPlayer height={"100%"} width={"100%"} url={url} />
+    <div className="relative w-[500px] h-[350px] overflow-hidden">
+      <ReactPlayer
+        url={url}
+        className="absolute left-0 top-0"
+        controls
+        width={"100%"}
+        height={"100%"}
+      />
     </div>
   );
 };
