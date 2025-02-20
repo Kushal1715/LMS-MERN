760864f406cc06d39036ddeeb2884b45ca22d3b4
Kushal1715 <kushalkhadka77@gmail.com>
2025-02-20 15:28:56 +0545

    making course controllers in server

---
+++ server/controllers/instructor-controller/course-controller/index.js
@@ -0,0 +1,39 @@
+const addNewCourse = async (req, res) => {
+  try {
+  } catch (e) {
+    return res.status(500).json({
+      success: false,
+      message: "some error occured",
+    });
+  }
+};
+
+const getAllCourses = async (req, res) => {
+  try {
+  } catch (e) {
+    return res.status(500).json({
+      success: false,
+      message: "some error occured",
+    });
+  }
+};
+
+const getCourseDetailsById = async (req, res) => {
+  try {
+  } catch (e) {
+    return res.status(500).json({
+      success: false,
+      message: "some error occured",
+    });
+  }
+};
+
+const updateCourseById = async (req, res) => {
+  try {
+  } catch (e) {
+    return res.status(500).json({
+      success: false,
+      message: "some error occured",
+    });
+  }
+};
