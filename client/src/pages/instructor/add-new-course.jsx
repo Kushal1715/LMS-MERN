d4ddcae275f0979b544d78abc891cce784eef560
Kushal1715 <kushalkhadka77@gmail.com>
2025-02-20 18:44:54 +0545

    validating form

--- client/src/pages/instructor/add-new-course.jsx
+++ client/src/pages/instructor/add-new-course.jsx
@@ -4,14 +4,53 @@ import CourseSettings from "@/components/instructor-view/courses/add-new-course/
 import { Button } from "@/components/ui/button";
 import { Card, CardContent } from "@/components/ui/card";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
-import React from "react";
+import { InstructorContext } from "@/context/instructor-context";
+import React, { useContext } from "react";
 
 const AddNewCourse = () => {
+  const { courseLandingFormData, courseCurriculumFormData } =
+    useContext(InstructorContext);
+
+  function isEmpty(value) {
+    if (Array.isArray(value)) {
+      return value.length === 0;
+    }
+
+    return value === "" || value === null || value === undefined;
+  }
+
+  function validateFormData() {
+    for (const key in courseLandingFormData) {
+      if (isEmpty(courseLandingFormData[key])) {
+        return false;
+      }
+    }
+
+    let hasFreePreview = false;
+
+    for (const item of courseCurriculumFormData) {
+      if (
+        isEmpty(item.title) ||
+        isEmpty(item.videoUrl) ||
+        isEmpty(item.public_id)
+      ) {
+        return false;
+      }
+
+      if (item.freePreview) {
+        hasFreePreview = true; //found at least one free preview
+      }
+    }
+
+    return hasFreePreview;
+  }
   return (
     <div className="container mx-auto p-4">
       <div className="flex justify-between items-center mb-8">
         <h1 className="font-extrabold text-3xl">Create New Course</h1>
-        <Button className="text-lg px-6">Submit</Button>
+        <Button className="text-lg px-6" disabled={!validateFormData()}>
+          Submit
+        </Button>
       </div>
       <Card className="p-8">
         <CardContent>
