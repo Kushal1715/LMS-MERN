80dcbcd7d948f81ec8ea2e17fd903462ffeea1cf
Kushal1715 <kushalkhadka77@gmail.com>
2025-02-23 21:09:04 +0545

    making student routes

---
+++ server/routes/student-routes/course-routes.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const {
+  getStudentViewCourses,
+  getStudentViewCourseDetails,
+} = require("../../controllers/student-controller/course-controller");
+
+const router = express.Router();
+
+router.get("/get", getStudentViewCourses);
+router.get("/get-details/:id", getStudentViewCourseDetails);
+
+module.exports = router;
