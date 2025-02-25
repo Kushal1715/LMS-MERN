c08d496a928e5a2e6091025c8afbc6e68e46f243
Kushal1715 <kushalkhadka77@gmail.com>
2025-02-25 14:00:22 +0545

    sort by ui completed in course page

--- client/src/pages/Student/courses/index.jsx
+++ client/src/pages/Student/courses/index.jsx
@@ -1,6 +1,15 @@
+import { Button } from "@/components/ui/button";
 import { Checkbox } from "@/components/ui/checkbox";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuRadioGroup,
+  DropdownMenuRadioItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
 import { Label } from "@/components/ui/label";
-import { filterOptions } from "@/config";
+import { filterOptions, sortOptions } from "@/config";
+import { ArrowUpDownIcon } from "lucide-react";
 import React from "react";
 
 const StudentViewCoursePage = () => {
@@ -11,11 +20,14 @@ const StudentViewCoursePage = () => {
         <aside className="w-full lg:w-72 space-y-4">
           <div className="space-y-4">
             {Object.keys(filterOptions).map((filterItem) => (
-              <div className="space-y-4">
+              <div className="space-y-4" key={filterItem}>
                 <h1 className="font-bold">{filterItem.toUpperCase()}</h1>
                 <div className="grid gap-2 mt-2">
                   {filterOptions[filterItem].map((option) => (
-                    <Label className="flex items-center gap-3">
+                    <Label
+                      className="flex items-center gap-3"
+                      key={option.label}
+                    >
                       <Checkbox />
                       {option.label}
                     </Label>
@@ -25,6 +37,34 @@ const StudentViewCoursePage = () => {
             ))}
           </div>
         </aside>
+        <main className="flex-1">
+          <div className="flex justify-end items-center gap-3">
+            <DropdownMenu>
+              <DropdownMenuTrigger asChild>
+                <Button variant="outline">
+                  <ArrowUpDownIcon />
+                  Sort by
+                </Button>
+              </DropdownMenuTrigger>
+              <DropdownMenuContent align="end" className="w-[180px]">
+                <DropdownMenuRadioGroup
+                //   value={sort}
+                //   onValueChange={(value) => setSort(value)}
+                >
+                  {sortOptions.map((sortItem) => (
+                    <DropdownMenuRadioItem
+                      value={sortItem.id}
+                      key={sortItem.id}
+                    >
+                      {sortItem.label}
+                    </DropdownMenuRadioItem>
+                  ))}
+                </DropdownMenuRadioGroup>
+              </DropdownMenuContent>
+            </DropdownMenu>
+            <span className="font-bold">10 Results</span>
+          </div>
+        </main>
       </div>
     </div>
   );
