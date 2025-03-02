55c3ae76fc03b29ab0cb5e06d2ab6cd975112deb
Kushal1715 <kushalkhadka77@gmail.com>
2025-03-02 20:25:25 +0545

    handling loading state in course-details page

--- client/src/pages/Student/course-details/index.jsx
+++ client/src/pages/Student/course-details/index.jsx
@@ -1,18 +1,22 @@
+import { Skeleton } from "@/components/ui/skeleton";
 import { StudentContext } from "@/context/student-context";
 import { studentViewGetCourseDetailsService } from "@/services";
 import React, { useContext, useEffect } from "react";
 import { useParams } from "react-router-dom";
 
 const StudentViewCourseDetailsPage = () => {
-  const { courseDetails, setCourseDetails } = useContext(StudentContext);
+  const { courseDetails, setCourseDetails, loading, setLoading } =
+    useContext(StudentContext);
   const params = useParams();
 
   const fetchCourseDetails = async (courseId) => {
     const response = await studentViewGetCourseDetailsService(courseId);
     if (response?.success) {
       setCourseDetails(response);
+      setLoading(false);
     } else {
       setCourseDetails(null);
+      setLoading(false);
     }
     console.log(response);
   };
@@ -20,6 +24,10 @@ const StudentViewCourseDetailsPage = () => {
   useEffect(() => {
     fetchCourseDetails(params.id);
   }, [params]);
+
+  if (loading) {
+    return <Skeleton />;
+  }
 
   return <div>StudentViewCourseDetailsPage</div>;
 };
