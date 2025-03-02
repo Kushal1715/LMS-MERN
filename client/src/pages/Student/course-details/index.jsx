a98353a5aa3f1c72cd5df391228fa9597dd8520a
Kushal1715 <kushalkhadka77@gmail.com>
2025-03-02 20:18:37 +0545

    using useEffect to extract course id from url

--- client/src/pages/Student/course-details/index.jsx
+++ client/src/pages/Student/course-details/index.jsx
@@ -1,8 +1,14 @@
 import { StudentContext } from "@/context/student-context";
-import React, { useContext } from "react";
+import React, { useContext, useEffect } from "react";
+import { useParams } from "react-router-dom";
 
 const StudentViewCourseDetailsPage = () => {
   const { courseDetails, setCourseDetails } = useContext(StudentContext);
+  const params = useParams();
+
+  useEffect(() => {
+    console.log(params);
+  }, [params]);
 
   return <div>StudentViewCourseDetailsPage</div>;
 };
