742f1bf5c25d5faada9ad88deb8eae967e9fd909
Kushal1715 <kushalkhadka77@gmail.com>
2025-03-09 20:54:57 +0545

    making controller to create order

---
+++ server/controllers/student-controller/order-controller/index.js
@@ -0,0 +1,112 @@
+const Order = require("../../../models/Order");
+
+const createOrder = async (req, res) => {
+  try {
+    const {
+      userId,
+      userName,
+      userEmail,
+      orderStatus,
+      paymentMethod,
+      paymentStatus,
+      orderDate: Date,
+      paymentId,
+      payerId,
+      instructorId,
+      instructorName,
+      courseImage,
+      courseTitle,
+      courseId,
+      coursePricing,
+    } = req.body;
+
+    const create_payment_json = {
+      intent: "sale",
+      payer: {
+        payment_method: "paypal",
+      },
+      redirect_urls: {
+        return_url: `${process.env.CLIENT_URL}/payment-return`,
+        cancel_url: `${process.env.CLIENT_URL}/payment-cancel`,
+      },
+      transactions: [
+        {
+          item_list: {
+            items: [
+              {
+                name: courseTitle,
+                sku: courseId,
+                price: coursePricing,
+                currency: "USD",
+                quantity: 1,
+              },
+            ],
+          },
+          amount: {
+            currency: "USD",
+            total: coursePricing.toFixed(2),
+          },
+          description: courseTitle,
+        },
+      ],
+    };
+
+    paypal.payment.create(create_payment_json, async (error, paymentInfo) => {
+      if (error) {
+        return res.status(500).json({
+          success: false,
+          message: "error while creating paypal payament",
+        });
+      } else {
+        const newlyCreatedCourseOrder = new Order({
+          userId,
+          userName,
+          userEmail,
+          orderStatus,
+          paymentMethod,
+          paymentStatus,
+          orderDate: Date,
+          paymentId,
+          payerId,
+          instructorId,
+          instructorName,
+          courseImage,
+          courseTitle,
+          courseId,
+          coursePricing,
+        });
+
+        await newlyCreatedCourseOrder.save();
+
+        const approveUrl = paymentInfo.links.find(
+          (link) => link.rel == "approval_url"
+        ).href;
+
+        res.status(201).json({
+          success: true,
+          data: {
+            approveUrl,
+            orderId: newlyCreatedCourseOrder._id,
+          },
+        });
+      }
+    });
+  } catch (e) {
+    res.status(500).json({
+      success: false,
+      message: "some error occured",
+    });
+  }
+};
+
+const capturePaymentAndFinalizeOrder = async (req, res) => {
+  try {
+  } catch (e) {
+    res.status(500).json({
+      success: false,
+      message: "some error occured",
+    });
+  }
+};
+
+module.exports = { createOrder, capturePaymentAndFinalizeOrder };
