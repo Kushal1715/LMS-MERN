d83ba7ae57f14d7f322bdad2feb4a515e321b748
Kushal1715 <kushalkhadka77@gmail.com>
2025-03-12 23:22:43 +0545

    making course progress controllers

---
+++ server/controllers/student-controller/courseProgress-controller/index.js
@@ -0,0 +1,41 @@
+//mark current lecture as viewed
+const markCurrentLectureAsViewed = async (req, res) => {
+  try {
+  } catch (e) {
+    console.log(e);
+    res.status(500).json({
+      success: false,
+      message: "some error occured",
+    });
+  }
+};
+
+//get current course progress
+const getCurrentCourseProgress = async (req, res) => {
+  try {
+  } catch (e) {
+    console.log(e);
+    res.status(500).json({
+      success: false,
+      message: "some error occured",
+    });
+  }
+};
+
+//reset course progress
+const resetCourseProgress = async (req, res) => {
+  try {
+  } catch (e) {
+    console.log(e);
+    res.status(500).json({
+      success: false,
+      message: "some error occured",
+    });
+  }
+};
+
+module.exports = {
+  markCurrentLectureAsViewed,
+  getCurrentCourseProgress,
+  resetCourseProgress,
+};
